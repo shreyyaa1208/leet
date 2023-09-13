8e12a85dbe01b9195893bc419630726f494bf221
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2023-09-14 00:05:49 +0530

    Time: 3 ms (37.94%), Space: 7.3 MB (5.8%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -13,25 +13,25 @@ public:
         int endingCol=col-1;
  while(count<total){
             //print startin row
-            for(int index=startingCol; count<total && index<=endingCol;index++){
+            for(int index=startingCol; count < total && index <= endingCol;index++){
                 ans.push_back(matrix[startingRow][index]);
                 count++;
             }
             startingRow++;
             //print ending col
-            for(int index=startingRow;count<total && index<=endingRow;index++){
+            for(int index=startingRow;count < total && index <= endingRow;index++){
                 ans.push_back(matrix[index][endingCol]);
                 count++;
             }
             endingCol--;
             //ending row
-            for(int index=endingCol;count<total && index>=startingCol;index--){
+            for(int index=endingCol;count<total && index >= startingCol;index--){
                 ans.push_back(matrix[endingRow][index]);
                 count++;
             }
             endingRow--;
             //print starting col
-            for(int index=endingRow;count<total && index>=startingRow;index--){
+            for(int index=endingRow;count<total && index >= startingRow;index--){
                 ans.push_back(matrix[index][startingCol]);
                 count++;  
             }
