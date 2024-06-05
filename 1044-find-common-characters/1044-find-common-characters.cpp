f21bc0b7990bdc9f08bf27328561f2cb705d7fe4
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2024-06-05 12:06:19 +0530

    Time: 10 ms (44.21%), Space: 11.6 MB (46.8%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -1,9 +1,21 @@
+/*
+Let's consider an example to understand the process:
+
+Input List: ["bella", "label", "roller"]
+Step 1: Count characters in "bella" -> last = [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Step 2: Intersection with "label"
+-> last = [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0] Step 3: Intersection with "roller" -> last = [1, 1, 0, 0, 1, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] Step 4: Construct result
+vector -> result = ["e", "l", "l"]
+*/
+
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
         // Initialize the frequency count for the first word
         vector<int> commonCount = countChars(words[0]);
-        
+
         // Update the frequency count to find common characters
         for (int i = 1; i < words.size(); i++) {
             vector<int> currentCount = countChars(words[i]);
@@ -12,7 +24,7 @@ public:
                 commonCount[j] = min(commonCount[j], currentCount[j]);
             }
         }
-        
+
         // Collect the common characters based on their counts
         vector<string> result;
         for (int i = 0; i < 26; i++) {
@@ -21,10 +33,10 @@ public:
                 commonCount[i]--;
             }
         }
-        
+
         return result;
     }
-    
+
 private:
     // Helper function to count the frequency of each character in a string
     vector<int> countChars(const string& str) {
