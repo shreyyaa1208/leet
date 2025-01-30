ad0f207f7749844d97b0c2cab85cc593d439f401
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2025-01-30 21:46:04 +0530

    Time: 8 ms (81.34%), Space: 11.8 MB (54.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,25 +6,17 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- /*
-BRUTE FORCE-
-create a hashmap 
-create temp pointer which will start from null
-traverse the temp pointer untill it reaches null
-while traversing store the elements in the hashmap
-when a element is present twice return true else false
- */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int>mpp;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow){
                 return true;
             }
-            mpp[temp]=1;
-            temp=temp->next;
         }
         return false;
     }
