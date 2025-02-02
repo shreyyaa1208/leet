040d0c29a60b8c4839cd1a676d1af121ed3a1578
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2025-02-02 23:13:57 +0530

    Time: 0 ms (100%), Space: 15 MB (25.23%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,53 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- /*
- BRUTE FORCE-
- divide into two sub-problems
- 1. count the total length
- 2.do(L-N+1)
- first w will count the total nodes till it reaches null
- once we have got the total count. Store it.
- now initialize temp pointer to the start 
- initialize a variable res=L-N
- start iterating the linked list while decrementing res at each node. 
- Once res equals 0, we know that temp will be pointing to the (L-N)th node, 
- therefore, stop the traversal.
- create a new link from (L-N)th node to (L-N+2)node
-
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL)
-        return NULL;
-        int count=0;
-ListNode* temp=head;
-while(temp!=NULL){
-count++;
-temp=temp->next;
-}
-if(count==n)//when the node to be deleted is the first node
-{
-    ListNode* newHead=head->next;
-    delete(head);
-    return newHead;
-}
-int res=count-n;
-temp=head;
-while (temp != NULL) {
-        res--;
-        if (res == 0) {
-            break;
-        }
-        temp = temp->next;
-    }
+        ListNode* fast=head;
+        ListNode* slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+            if(fast==NULL)
+            return head->next;
 
-ListNode* delNode=temp->next;
-temp->next=temp->next->next;
-delete(delNode);
-return head;
-        
+        }
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+           
+        }
+        ListNode* delNode=slow->next;
+        slow->next=slow->next->next;
+        delete delNode;
+        return head;
     }
 };
