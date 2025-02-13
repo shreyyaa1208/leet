8e7ccc44d0112537d6abd84bc1d59554dac650ed
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2025-02-14 01:34:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int>counts(26,0);
+        int res=0;
+        int =0;
+        int r=0;
+        int ans=0;
+    }
+};
