5c6df78451edfb7568baeeb5157382d06700c072
shreyyaa1208 <91733422+shreyyaa1208@users.noreply.github.com>
2025-02-16 00:28:03 +0530

    Time: 4 ms (65.94%), Space: 10.9 MB (43.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int>counts(26,0);
-        int res=0;
-        int =0;
-        int r=0;
-        int ans=0;
+       vector<int>counts(26,0);
+       int res=0;
+       int r=0;
+       int l=0;
+        int count=0;
+        int n=s.length();
+        for( r=0;r<n;r++){
+          counts [s[r]-'A']++;
+          if(count<counts[s[r]-'A']){
+            count=counts[s[r]-'A'];
+          }
+          while(r-l-count+1>k){
+            counts[s[l]-'A']--;
+            l++;
+            for(int i=0;i<26;i++){
+             if(count<counts[i]) {
+                count=counts[i];
+             }  
+            }
+          }
+          res=max(res,r-l+1);
+        }
+        return res;
     }
 };
